dcb095f4b6c9769e8fcff352b8c70c2539e499bb
Dylan Zhang <534929089@qq.com>
2025-12-18 17:05:46 +0800

    11

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import '../l10n/app_localizations.dart';
 import '../models/image_item.dart';
 import '../services/image_service.dart';
 import '../widgets/image_list_table.dart';
